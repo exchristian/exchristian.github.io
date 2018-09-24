b86a4e57dcffc4b63bb3cff784b60dd7a1b62bb1
wuyiqin18 <wuyiqin18@gmail.com>
2018-09-24 10:02:11 +0100

    update

--- copy.sh
+++ copy.sh
@@ -2,8 +2,8 @@ gitbook install src
 gitbook build src docs
 
 
-gitbook pdf src docs/mybook.pdf
-gitbook epub src docs/mybook.epub
-gitbook mobi src docs/mybook.mobi
+gitbook pdf src ./mybook.pdf
+gitbook epub src ./mybook.epub
+gitbook mobi src ./mybook.mobi
 
 cp -r docs/* ./
